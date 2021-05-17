b4d924adf6784fc1c5fcefe64f363e3194559f29
Giancarlo <giancarlorondo@outlook.com>
2021-05-17 17:27:50 +0200

    menu-toggle

--- src/components/MenuToggle/MenuToggle.jsx
+++ src/components/MenuToggle/MenuToggle.jsx
@@ -1,13 +1,36 @@
 import React,{ useState } from 'react';
+import { Nav, NavItem } from 'reactstrap';
+
+import classNames from 'classnames';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const MenuToggle = (props) => {
+const MenuToggle = () => {
+
+    let [state, setState] = useState({
+        isOpen: false
+    });
+
+    const toggleMenu = () => {
+        setState({ isOpen: !state.isOpen});
+    };
+
     return(
         <div className="menuToggle">
-            <div className="menuToggle-icon">
+            <div className="menuToggle-icon" onClick={toggleMenu}>
                 <FontAwesomeIcon icon={faBars} />
+            </div>
+            <div className={classNames("sidebar", {"is-open": state.isOpen})}>
+                <FontAwesomeIcon icon={faTimes} onClick={toggleMenu} className="close-icon"/>
+                <Nav>
+                    <NavItem>Particulares</NavItem>
+                    <NavItem>Cuentas y tarjetas</NavItem>
+                    <NavItem>Ahorro e Inversión</NavItem>
+                    <NavItem>Financiación</NavItem>
+                    <NavItem>Seguros</NavItem>
+                    <NavItem>Ofertas Especiales</NavItem>
+                </Nav>
             </div>
         </div>
     );
